0e094e56b5c10735644e00c533a58ded3797a107
joilsonlira <lirajoilson@gmail.com>
2023-05-05 18:53:26 -0300

    base

--- src/routes/Terapia.jsx
+++ src/routes/Terapia.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Section_container } from "../components/Section_container";
 
 const Terapia = ()=>{
     return(
-        <div>
-            <h2>Terapia</h2>
-        </div>
+        <>
+            <Section_container
+            divs={["info", "imagem"]}
+            isFirst={"first"}
+            isHighlights={""}
+            imagem={"./imgs/empresaria.png"} 
+            direction={"row"}  
+            width={"35%"} 
+            text_align={"left"} 
+            title={"Título que convença o visitante a continuar lendo"} 
+            paragraph={"Subtítulo que desenvolva a sua proposta única de venda"} />        
+        </>
     )
 }
 export default Terapia;
